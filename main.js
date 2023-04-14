315bc28a659a602a55dde864beebb3655adc1bc8
Aghiles Haddar <haddaraghiles5@gmail.com>
2023-04-14 22:34:35 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const $h1 = document.querySelector('h1')
+
+$h1.addEventListener('click', function() {
+    window.location.href = 'https://www.google.com/finance/quote/ULVR:LON?sa=X&ved=2ahUKEwjb8_-hv5L-AhVIVKQEHcHfBdEQ3ecFegQIGBAg';
+});
+
+console.log('izan')
